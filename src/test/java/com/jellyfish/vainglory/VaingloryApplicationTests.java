a7a7d15add131f62d89c18fee9b1783c28167498
lid.guan <lid.guan@gmail.com>
2025-03-30 23:30:38 +0800

    init

---
+++ src/test/java/com/jellyfish/vainglory/VaingloryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jellyfish.vainglory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaingloryApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
