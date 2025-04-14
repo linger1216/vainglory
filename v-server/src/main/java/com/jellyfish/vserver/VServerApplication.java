dc5bd68b8a1976fb9cef8888ccb956a6aa054e82
lid.guan <lid.guan@gmail.com>
2025-04-14 19:29:22 +0800

    init again

---
+++ v-server/src/main/java/com/jellyfish/vserver/VServerApplication.java
@@ -0,0 +1,13 @@
+package com.jellyfish.vserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VServerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(VServerApplication.class, args);
+  }
+
+}
